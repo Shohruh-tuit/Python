5a8cc008a1935153077e8c8258404b178507b683
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-02-13 23:57:28 +0500

    daa

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
-print(3 % 2)
+# print(5 % 3)
+
+# name = input("Как вас зовут?")
+# print("Привет" + name)
+num_1 = int(input("Enter first number: "))
+num_2 =    input("Enter first number: ")
+res = num_1 + num_2
+print(res)
 
