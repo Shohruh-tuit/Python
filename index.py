f60cb40023afc50f1a557498c8b80a4e8f19e549
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-02-14 00:44:25 +0500

    daa

--- index.py
+++ index.py
@@ -3,7 +3,10 @@
 # name = input("Как вас зовут?")
 # print("Привет" + name)
 num_1 = int(input("Enter first number: "))
-num_2 =    input("Enter first number: ")
-res = num_1 + num_2
+num_2 = input("Enter first number: ")
+res = num_1 + int(num_2)
+Res = 'Shohruh'
+Res *= 5
+print(Res)
 print(res)
 
