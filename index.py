61091c8923e321d0a6b88e14f25c28b4ae777c73
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-02-14 18:47:13 +0500

    daa

--- index.py
+++ index.py
@@ -1,12 +1,38 @@
-# print(5 % 3)
 
 # name = input("Как вас зовут?")
 # print("Привет" + name)
-num_1 = int(input("Enter first number: "))
-num_2 = input("Enter first number: ")
-res = num_1 + int(num_2)
-Res = 'Shohruh'
-Res *= 5
-print(Res)
-print(res)
+# num_1 = int(input("Enter first number: "))
+# num_2 = input("Enter first number: ")
+# res = num_1 + int(num_2)
+# Res = 'Shohruh'
+# Res *= 5
+# print(Res)
+# print(res)
 
+
+
+# Shartli operator
+# num = input("What is your name?: ")
+# if num == "Shohruh":
+#     print("Salom ho'jayin\n")
+# elif num == "Baxodir":
+#     print("Salom Shohruhning adasi ")
+# else: print("You are not Shohruh!!!")
+
+
+# number = int(input("Enter number: "))
+
+# if number > 0:
+#     print("Siz 0 dan katta son kiritdingiz!!!")
+#     if number > 10:
+#         print("SIz 10 dan ham katta son kiritdingiz")
+#     else: print("Siz 10 dan kichik lekin 0 dan katta son kirtdingiz")
+# else:
+#     print("Siz 0 dan kichik son kiritdingiz")
+
+# name = input("Mevaning ismini kiriting: ")
+# A = "Ha Olma" if name == "Olma" else "Olma emas"
+# print(A)
+
+import random
+print(random.randrange(80,100))
